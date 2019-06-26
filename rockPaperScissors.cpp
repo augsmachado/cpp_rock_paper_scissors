875fa129371e7fdb6e8c09a8f4dcd6b36bae8156
Augs Machado <augustomachadoramos@gmail.com>
2019-06-26 19:38:14 -0300

    Update rockPaperScissors.cpp
    It is necessary to remodel conditions to know who won the round.

--- rockPaperScissors.cpp
+++ rockPaperScissors.cpp
@@ -22,22 +22,50 @@ int main () {
         randomOptionSys(&optionSys);
 
         if (optionSys >= 1 && optionSys <= 33){  // Sys equal the rock
+            
             if (optionUser == 2){
+                std::cout << "Rock loses for paper" << endl;
                 pointUser++;
-            } else if (){
-                
+
+            } else if (optionSys == 3){
+                std::cout << "Rock wins scissors" << endl;
+                pointSys++;
             }
+            
+            
         } else if (optionSys >= 34 && optionSys <= 66){ // Sys equal the paper
+            
+            if (optionUser == 2){
+                std::cout << "Paper loses for scissors" << endl;
+                pointUser++;
+            
+            } else if (optionSys == 3){
+                std::cout << "Paper wins rock" << endl;
+                pointSys++;
+            }
+            
             
         } else if (optionSys >= 67 && optionSys <= 100){  // Sys equal the scissors
             
+            if (optionUser == 2){
+                std::cout << "Scissors loses for rock" << endl;
+                pointUser++;
+            
+            } else if (optionSys == 3){
+                std::cout << "Scissors wins paper" << endl;
+                pointSys++;
+            }
         }
-
-
-
 
         round++;
     }
+    
+    if(pointSys > pointUser){
+        std::cout << "Sorry, but the system won" << std::endl;
+    } else {
+        std::cout << "Congratulations, you won!" << std::endl;
+    }
+    
     return 0;
 }
 
