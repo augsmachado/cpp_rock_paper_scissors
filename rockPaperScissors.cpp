3d714fdb2e75e20ea643b398815657db40c4064b
Augs Machado <augustomachadoramos@gmail.com>
2019-06-27 18:45:50 -0300

    Game functional

--- rockPaperScissors.cpp
+++ rockPaperScissors.cpp
@@ -1,14 +1,45 @@
 # include <iostream>
 # include <cstdlib>
 # include <ctime>
+# include <stdlib.h>
 using namespace std;
 
 void menuOption ();
 void inputOptionUser( int *optionUser);
 void randomOptionSys (int *optionSys);
+int match ();
 
 
 int main () {
+    char newMatch;
+    int winner;
+    int winUserMatch = 0, winSysMatch = 0;
+    
+    std::cout << "Press Y to start game" << endl;
+    std::cin >> newMatch;
+    
+    while (newMatch == 'Y'){
+        
+        winner = match();
+        
+        if (winner == true){
+            winSysMatch++;
+        } else {
+            winUserMatch++;
+        }
+        
+        cout << "Want to play again? Yes [Y] or no [n]?" << endl;
+        cin >> newMatch;
+        
+    }
+    
+    cout << "System: " << winSysMatch <<" vs. You: " << winUserMatch << endl;
+    
+    return 0;
+}
+
+
+int match () {
     int optionUser, optionSys;
     int pointUser = 0, pointSys = 0;
     int round = 1;
@@ -21,53 +52,52 @@ int main () {
         inputOptionUser(&optionUser);
         randomOptionSys(&optionSys);
 
-		/*
-		* Options to user:
-    	* 1 - Rock
-    	* 2 - Paper
-    	* 3 - Scissors
-		*/
-
 
         if (optionSys >= 1 && optionSys <= 33){  // Sys equal the rock
             
+            // If system equal the rock, lose to 2-paper, win 3 scissor
             if (optionUser == 2){
                 std::cout << "Rock loses for paper" << endl;
+                std::cout << "You won" << endl;
                 pointUser++;
-
-            }
-			
-			if (optionUser == 3){
+            } else if (optionUser == 3){
                 std::cout << "Rock wins scissors" << endl;
+                std::cout << "You lost" << endl;
                 pointSys++;
+            } else {
+                std::cout << "Gave a tie" << endl;
             }
             
             
         } else if (optionSys >= 34 && optionSys <= 66){ // Sys equal the paper
             
+            // If system equal the paper, lose to 3-scissors, win 1-rock
             if (optionUser == 3){
                 std::cout << "Paper loses for scissors" << endl;
+                std::cout << "You won" << endl;
                 pointUser++;
-            
-            }
-			
-			if (optionUser == 1){
+            } else if (optionUser == 1){
                 std::cout << "Paper wins rock" << endl;
+                std::cout << "You lost" << endl;
                 pointSys++;
+            } else {
+                std::cout << "Gave a tie" << endl;
             }
             
             
         } else if (optionSys >= 67 && optionSys <= 100){  // Sys equal the scissors
-            
-            if (optionUser == 3){
+        
+            // If system equal the scissors, lose to 1-rock, win 2-paper
+            if (optionUser == 1){
                 std::cout << "Scissors loses for rock" << endl;
+                std::cout << "You won" << endl;
                 pointUser++;
-            
-            }
-			
-			if (optionUser == 3){
+            } else if (optionUser == 2){
                 std::cout << "Scissors wins paper" << endl;
+                std::cout << "You lost" << endl;
                 pointSys++;
+            } else {
+                std::cout << "Gave a tie" << endl;
             }
         }
 
@@ -76,11 +106,11 @@ int main () {
     
     if(pointSys > pointUser){
         std::cout << "Sorry, but the system won" << std::endl;
+        return true;
     } else {
         std::cout << "Congratulations, you won!" << std::endl;
+        return false;
     }
-    
-    return 0;
 }
 
 
@@ -91,11 +121,13 @@ void menuOption (){
     std::cout << "3 - Scissors" << std::endl;
 }
 
+
 void inputOptionUser( int *optionUser){
     while (*optionUser < 1 || *optionUser > 3){
         std::cin >> *optionUser;
     }
 }
+
 
 void randomOptionSys (int *optionSys){
     *optionSys = (1 + (rand() % 100));
